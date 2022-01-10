9d4d9f530d60b38ecce08f6f51ea214784d70628
Mansoor <ali.syedmansoor1997@gmail.com>
2022-01-10 15:18:29 +0530

    Image Download Fix

--- script.js
+++ script.js
@@ -1,9 +1,20 @@
 document.getElementById("download").addEventListener("click", () => {
-  var node = document.getElementById("capture");
-
   htmlToImage
     .toPng(document.getElementById("capture"))
     .then(function (dataUrl) {
-      download(dataUrl, "my-node.png");
+      saveAs(dataUrl, "iced.png");
     });
 });
+
+function saveAs(uri, filename) {
+  var link = document.createElement("a");
+  if (typeof link.download === "string") {
+    link.href = uri;
+    link.download = filename;
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
+  } else {
+    window.open(uri);
+  }
+}
