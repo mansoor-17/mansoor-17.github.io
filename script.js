679639176ecfd83da53a87f43c83ce42a6c8c968
Mansoor <ali.syedmansoor1997@gmail.com>
2022-01-26 23:49:30 +0530

    Two Thumbs working

--- script.js
+++ script.js
@@ -36,3 +36,30 @@ function saveAs(uri, filename) {
     window.open(uri);
   }
 }
+
+const allSliders = document.querySelectorAll(".range-slider");
+allSliders.forEach((ranges) => {
+  const sliders = ranges.querySelectorAll(".range");
+  let s1 = sliders[0];
+  let current = ranges.querySelector(".current");
+  let s2 = sliders[1];
+  let news = ranges.querySelector(".new");
+  s1.addEventListener("input", () => {
+    setBubble(s1, current);
+  });
+  setBubble(s1, current);
+  s2.addEventListener("input", () => {
+    setBubble(s2, news);
+  });
+  setBubble(s2, news);
+});
+
+function setBubble(range, bubble) {
+  const val = range.value;
+  const min = range.min ? range.min : 0;
+  const max = range.max ? range.max : 100;
+  const newVal = Number(((val - min) * 100) / (max - min));
+
+  // Sorta magic numbers based on size of the native UI thumb
+  bubble.style.left = `calc(${newVal}% + (${8 - newVal * 0.15}px))`;
+}
